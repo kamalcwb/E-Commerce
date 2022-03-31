7948fb2a8cb9910fccc48a2176f879fdf79981ec
ADELSON SILVA DE PAULA <69219101+kamalcwb@users.noreply.github.com>
2022-03-30 21:51:57 -0300

    Área de produtos.
    Adicionado área de produtos.

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,6 +3,7 @@ import React from 'react'
 import Slider from '../components/Slider'
 import Navbar from '../components/Navbar'
 import Categories from '../components/Categories'
+import Products from '../components/Products'
 
 function Home() {
     return (
@@ -11,6 +12,7 @@ function Home() {
             <Navbar />
             <Slider />
             <Categories />
+            <Products />
         </div>
     )
 }
