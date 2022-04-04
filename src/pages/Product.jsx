5e7fd45350d772f84ce6d6df3751fecbc841fcd9
ADELSON SILVA DE PAULA <69219101+kamalcwb@users.noreply.github.com>
2022-04-03 23:33:28 -0300

    Pagina de produto
    Adicionado modelo de pagina de produto.

---
+++ src/pages/Product.jsx
@@ -0,0 +1,172 @@
+import { Add, Remove } from "@material-ui/icons";
+import styled from "styled-components";
+import Announcement from "../components/Announcement";
+import Footer from "../components/Footer";
+import Navbar from "../components/Navbar";
+import Newsletter from "../components/Newsletter";
+
+const Container = styled.div``;
+
+const Wrapper = styled.div`
+  padding: 50px;
+  display: flex;
+`;
+
+const ImgContainer = styled.div`
+  flex: 1;
+`;
+
+const Image = styled.img`
+  width: 100%;
+  height: 90vh;
+  object-fit: cover;
+`;
+
+const InfoContainer = styled.div`
+  flex: 1;
+  padding: 0px 50px;
+`;
+
+const Title = styled.h1`
+  font-weight: 200;
+`;
+
+const Desc = styled.p`
+  margin: 20px 0px;
+`;
+
+const Price = styled.span`
+  font-weight: 100;
+  font-size: 40px;
+`;
+
+const FilterContainer = styled.div`
+  width: 50%;
+  margin: 30px 0px;
+  display: flex;
+  justify-content: space-between;
+`;
+
+const Filter = styled.div`
+  display: flex;
+  align-items: center;
+`;
+
+const FilterTitle = styled.span`
+  font-size: 20px;
+  font-weight: 200;
+`;
+
+const FilterColor = styled.div`
+  width: 20px;
+  height: 20px;
+  border-radius: 50%;
+  background-color: ${(props) => props.color};
+  margin: 0px 5px;
+  cursor: pointer;
+`;
+
+const FilterSize = styled.select`
+  margin-left: 10px;
+  padding: 5px;
+`;
+
+const FilterSizeOption = styled.option``;
+
+const AddContainer = styled.div`
+  width: 50%;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+`;
+
+const AmountContainer = styled.div`
+  display: flex;
+  align-items: center;
+  font-weight: 700;
+`;
+
+const Amount = styled.span`
+  width: 30px;
+  height: 30px;
+  border-radius: 10px;
+  border: 1px solid teal;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  margin: 0px 5px;
+`;
+
+const Button = styled.button`
+  padding: 15px;
+  border: 2px solid teal;
+  background-color: white;
+  cursor: pointer;
+  font-weight: 500;
+
+  &:hover{
+      background-color: #f8f4f4;
+  }
+`;
+
+const Product = () => {
+    return (
+        <Container>
+            <Navbar />
+            <Announcement />
+            <Wrapper>
+                <ImgContainer>
+                    <Image src="https://i.ibb.co/S6qMxwr/jean.jpg" />
+                </ImgContainer>
+                <InfoContainer>
+                    <Title>Macacão Jeans Delavê Denim Claro</Title>
+                    <Desc>
+                        Macacão confeccionado em jeans com lavagem delavê. Modelo sem manga, conta com decote reto,
+                        fechamento frontal por botões, alças com amarração, bolsos frontais e bolsos posteriores. Cheio de estilo,
+                        é perfeito para compor looks super descolados. <br /><br />
+                        • Bolsos frontais <br /><br />
+                        • Alças ajustáveis <br /><br />
+                        <strong>JEANS + TRANSPARENTE</strong> <br /><br />
+                        <strong>O futuro está em nossas mãos.</strong> O <strong>Jeans + Transparente </strong>
+                        representa o início de um novo capítulo na <strong>história</strong> de um ícone da moda. <br /> A responsabilidade
+                        com o <strong>meio ambiente</strong> e suas comunidades é o que guia a criação de cada peça.
+                        Para produzir cada vez <strong>melhor</strong>, preservamos cada vez mais nossos
+                        <strong>recursos naturais</strong>. Esse é o jeito de fazer <strong>moda</strong> em que acreditamos:
+                        Nossa forma de <strong>abraçar o planeta</strong> e as gerações que estão por vir.
+                    </Desc>
+                    <Price>R$ 159,90</Price>
+                    <FilterContainer>
+                        <Filter>
+                            <FilterTitle>Cor</FilterTitle>
+                            <FilterColor color="black" />
+                            <FilterColor color="darkblue" />
+                            <FilterColor color="gray" />
+                        </Filter>
+                        <Filter>
+                            <FilterTitle>Tamanho</FilterTitle>
+                            <FilterSize>
+                                <FilterSizeOption>PP</FilterSizeOption>
+                                <FilterSizeOption>P</FilterSizeOption>
+                                <FilterSizeOption>M</FilterSizeOption>
+                                <FilterSizeOption>G</FilterSizeOption>
+                                <FilterSizeOption>GG</FilterSizeOption>
+                            </FilterSize>
+                        </Filter>
+                    </FilterContainer>
+                    <AddContainer>
+                        <AmountContainer>
+                            <Remove />
+                            <Amount>1</Amount>
+                            <Add />
+                        </AmountContainer>
+                        <Button>ADICIONAR AO CARRINHO</Button>
+                    </AddContainer>
+                </InfoContainer>
+            </Wrapper>
+            <Newsletter />
+            <Footer />
+        </Container>
+    );
+};
+
+export default Product;
