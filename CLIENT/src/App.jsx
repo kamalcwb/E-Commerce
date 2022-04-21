bf46d7f200c4c616b3c854fec26fdc8856d48bc1
ADELSON SILVA DE PAULA <69219101+kamalcwb@users.noreply.github.com>
2022-04-21 19:07:09 -0300

    Fix
    Concertado alguns bugs nas rotas

--- CLIENT/src/App.jsx
+++ CLIENT/src/App.jsx
@@ -4,37 +4,28 @@ import Product from "./pages/Product";
 import Register from "./pages/Register";
 import Login from "./pages/Login";
 import Cart from "./pages/Cart"
+import { render } from "react-dom";
 import {
-  BrowserRouter as Router,
+  BrowserRouter,
   Routes,
   Route,
-  Navigate
-} from "react-router-dom"
+} from "react-router-dom";
 
 const App = () => {
-  const user = true;
-  return <Router>
-    <Routes>
-      <Route exact path="/">
-        <Home />
-      </Route>
-      <Route path="/products/:category">
-        <ProductList />
-      </Route>
-      <Route path="/product/:id">
-        <Product />
-      </Route>
-      <Route path="/cart">
-        <Cart />
-      </Route>
-      <Route patch="/login">
-        {user ? <Navigate to="/" /> : <Login />}
-      </Route>
-      <Route path="/register">
-        {user ? <Navigate to="/" /> : <Register />}
-      </Route>
-    </Routes>
-  </Router>
+  return render(
+    <BrowserRouter>
+      <Routes>
+        <Route exact path="/" element={<Home />} />
+        <Route path="/produtos/:category" element={<ProductList />} />
+        <Route path="/produto/:id" element={<Product />} />
+        <Route path="/carrinho" element={<Cart />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/registro" element={<Register />} />
+        <Route path="*" element={<Home />} />
+      </Routes>
+    </BrowserRouter>,
+    document.getElementById("root")
+  );
 };
 
 export default App;
