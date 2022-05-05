a502394ea266e9aa8d7dc86216667d8c4012c463
ADELSON SILVA DE PAULA <69219101+kamalcwb@users.noreply.github.com>
2022-05-05 20:42:26 -0300

    update

--- CLIENT/src/components/Announcement.jsx
+++ CLIENT/src/components/Announcement.jsx
@@ -4,7 +4,6 @@ import styled from 'styled-components'
 const Container = styled.div`
     height: 30px;
     background-color: lightgray;
-    color: white;
     display: flex;
     align-items: center;
     justify-content: center;
