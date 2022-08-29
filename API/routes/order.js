c884d95d5fd8213930d1aa6204819299f687b157
ADELSON SILVA DE PAULA <69219101+kamalcwb@users.noreply.github.com>
2022-08-29 17:38:53 -0300

    add api

---
+++ API/routes/order.js
@@ -0,0 +1,107 @@
+const Order = require("../models/Order");
+const {
+    verifyToken,
+    verifyTokenAndAuthorization,
+    verifyTokenAndAdmin,
+} = require("./verifyToken");
+
+const router = require("express").Router();
+
+//CRIAR
+
+router.post("/", verifyToken, async (req, res) => {
+    const newOrder = new Order(req.body);
+
+    try {
+        const savedOrder = await newOrder.save();
+        res.status(200).json(savedOrder);
+    } catch (err) {
+        res.status(500).json(err);
+    }
+});
+
+//ATT
+router.put("/:id", verifyTokenAndAdmin, async (req, res) => {
+    try {
+        const updatedOrder = await Order.findByIdAndUpdate(
+            req.params.id,
+            {
+                $set: req.body,
+            },
+            { new: true }
+        );
+        res.status(200).json(updatedOrder);
+    } catch (err) {
+        res.status(500).json(err);
+    }
+});
+
+//DELETAR
+router.delete("/:id", verifyTokenAndAdmin, async (req, res) => {
+    try {
+        await Order.findByIdAndDelete(req.params.id);
+        res.status(200).json("Pedido excluído.");
+    } catch (err) {
+        res.status(500).json(err);
+    }
+});
+
+//PEDIDOS DE USUÁRIOS
+router.get("/find/:userId", verifyTokenAndAuthorization, async (req, res) => {
+    try {
+        const orders = await Order.find({ userId: req.params.userId });
+        res.status(200).json(orders);
+    } catch (err) {
+        res.status(500).json(err);
+    }
+});
+
+// //TODOS
+
+router.get("/", verifyTokenAndAdmin, async (req, res) => {
+    try {
+        const orders = await Order.find();
+        res.status(200).json(orders);
+    } catch (err) {
+        res.status(500).json(err);
+    }
+});
+
+// RENDA MENSAL
+
+router.get("/income", verifyTokenAndAdmin, async (req, res) => {
+    const productId = req.query.pid;
+    const date = new Date();
+    const lastMonth = new Date(date.setMonth(date.getMonth() - 1));
+    const previousMonth = new Date(new Date().setMonth(lastMonth.getMonth() - 1));
+
+    try {
+        const income = await Order.aggregate([
+            {
+                $match: {
+                    createdAt: { $gte: previousMonth },
+                    ...(productId && {
+                        products: { $elemMatch: { productId } },
+                    }),
+                },
+            },
+            {
+                $project: {
+                    month: { $month: "$createdAt" },
+                    sales: "$amount",
+                },
+            },
+            {
+                $group: {
+                    _id: "$month",
+                    total: { $sum: "$sales" },
+                },
+            },
+        ]);
+        res.status(200).json(income);
+    } catch (err) {
+        res.status(500).json(err);
+    }
+});
+
+module.exports = router;
